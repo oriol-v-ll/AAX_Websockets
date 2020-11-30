23f89e9d4166449a55f359ede277b66801202eec
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-11-30 21:30:19 +0100

    errores

--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -21,17 +21,19 @@ function onMessage(event) {
     }
 }
 
-function addDevice(name, type, description) {
-    var DeviceAction = {
-        action: "add",
-        name: name,
-        type: type,
-        description: description
-    };
-    socket.send(JSON.stringify(DeviceAction));
+
+function addComparation(name1, name2, type1, type2){
+	var CompareKPI = {
+        action: "compare",
+        name1: name1,
+        type1: type1,
+        name2: name2,
+        type2: type2,
+    }; 
+ socket.send(JSON.stringify(CompareKPI));
 }
 
-function removeDevice(element) {
+function removeKPI(element) {
     var id = element;
     var DeviceAction = {
         action: "remove",
@@ -40,48 +42,44 @@ function removeDevice(element) {
     socket.send(JSON.stringify(DeviceAction));
 }
 
-function toggleDevice(element) {
-    var id = element;
-    var DeviceAction = {
-        action: "toggle",
-        id: id
-    };
-    socket.send(JSON.stringify(DeviceAction));
-}
 
-function printDeviceElement(device) {
-    var content = document.getElementById("content");
+
+function printKPIComparationElement(kpi){
+	var content = document.getElementById("content");
+	
+	var kpiDiv = document.createElement("div");
+	kpiDiv.setAttribute("id", kpiDiv.id);
+	kpiDiv.setAttribute("class", "kpi" + kpi.type);
+    content.appendChild(kpiDiv);
     
-    var deviceDiv = document.createElement("div");
-    deviceDiv.setAttribute("id", device.id);
-    deviceDiv.setAttribute("class", "device " + device.type);
-    content.appendChild(deviceDiv);
+    var kpiName1 = document.createElement("span");
+    kpiName1.setAttribute("class", "kpiName1");
+    kpiName1.innerHTML = kpi.name;
+    kpiDiv.appendChild(kpiName1);
 
-    var deviceName = document.createElement("span");
-    deviceName.setAttribute("class", "deviceName");
-    deviceName.innerHTML = device.name;
-    deviceDiv.appendChild(deviceName);
-
-    var deviceType = document.createElement("span");
-    deviceType.innerHTML = "<b>Type:</b> " + device.type;
-    deviceDiv.appendChild(deviceType);
-
-    var deviceStatus = document.createElement("span");
-    if (device.status === "On") {
-        deviceStatus.innerHTML = "<b>Status:</b> " + device.status + " (<a href=\"#\" OnClick=toggleDevice(" + device.id + ")>Turn off</a>)";
-    } else if (device.status === "Off") {
-        deviceStatus.innerHTML = "<b>Status:</b> " + device.status + " (<a href=\"#\" OnClick=toggleDevice(" + device.id + ")>Turn on</a>)";
-    }
-    deviceDiv.appendChild(deviceStatus);
-
-    var deviceDescription = document.createElement("span");
-    deviceDescription.innerHTML = "<b>Comments:</b> " + device.description;
-    deviceDiv.appendChild(deviceDescription);
-
-    var removeDevice = document.createElement("span");
-    removeDevice.setAttribute("class", "removeDevice");
-    removeDevice.innerHTML = "<a href=\"#\" OnClick=removeDevice(" + device.id + ")>Remove device</a>";
-    deviceDiv.appendChild(removeDevice);
+    var kpiType = document.createElement("span");
+    kpiType.innerHTML = "<b>Type:</b> " + device.type;
+    kpiDiv.appendChild(kpiType);
+    
+    var kpiName2 = document.createElement("span");
+    kpiName2.setAttribute("class", "kpiName2");
+    kpiName2.innerHTML = kpi.name2;
+    kpiDiv.appendChild(kpiName2);
+    
+    var kpiType = document.createElement("span");
+    kpiType.innerHTML = "<b>Type:</b> " + device.type2;
+    kpiDiv.appendChild(kpiType);
+    
+    /*Creación del sistema del numero aleatorio + hora*/
+    
+    /*Creación del sistema del numero aleatorio + hora*/
+    
+    var removeKpi = document.createElement("span");
+    removeKpi.setAttribute("class", "removeKpi");
+    removeKpi.innerHTML = "<a href=\"#\" OnClick=removeKPI(" + kpi.id + ")>Remove comparation</a>";
+    kpiDiv.appendChild(removeKpi);
+    
+	
 }
 
 function showForm() {
@@ -94,10 +92,22 @@ function hideForm() {
 
 function formSubmit() {
     var form = document.getElementById("addDeviceForm");
+	var name1 = form.elements["device_name1"].value;
+	var type1 = form.elements["device_type1"].value;
+    var name2 = form.elements["device_name2"].value;
+    var type2 = form.elements["device_type2"].value;
+    hideForm();
+    document.getElementById("addDeviceForm").reset();
+    addComparation(name1,name2,type1,type2);
+}
+
+function formSubmit() {
+	hideForm();
+    var form = document.getElementById("addDeviceForm");
     var name = form.elements["device_name"].value;
     var type = form.elements["device_type"].value;
     var description = form.elements["device_description"].value;
-    hideForm();
+    
     document.getElementById("addDeviceForm").reset();
     addDevice(name, type, description);
 }
