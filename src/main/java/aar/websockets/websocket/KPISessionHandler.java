141d85b94a8675c39040a107b0e945cb303b1c55
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-12-05 20:05:22 +0100

    Frontend terminado

--- src/main/java/aar/websockets/websocket/KPISessionHandler.java
+++ src/main/java/aar/websockets/websocket/KPISessionHandler.java
@@ -2,10 +2,9 @@ package aar.websockets.websocket;
 
 import java.io.IOException;
 
-import java.text.DateFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
-import java.util.Locale;
+
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,148 +14,125 @@ import javax.json.JsonObject;
 import javax.json.spi.JsonProvider;
 import javax.websocket.Session;
 
-
 import aar.websockets.model.KPI;
-
 
 @ApplicationScoped
 public class KPISessionHandler {
-	 	private int deviceId = 0;
-	    private final static Set<Session> sessions = new HashSet<>();
-	    private final static Set<KPI> Kpis = new HashSet<>();
-	    private int TIEMPO = 5000; //en milisegundos
-	    
-	    public  KPISessionHandler() {
-	    	 System.out.println("hello world!");
-	    	 //Thread
-	    	 main();
-	    }
-	    public void main() {
-	    	 Runnable runnable = new Runnable() {
-	    		 @Override
-	    		 public void run() {
-	    			 while (true) {
-	    				 try {
-	    					 Thread.sleep(TIEMPO);
-	    					 KPISessionHandler.actualizarValorAleatorio();
-	    				 }catch (InterruptedException e) {
-	    					 e.printStackTrace();
-	    				 }
-	    			 }
-	    		 }
-	    	 };
-	    	 
-	    	 new Thread(runnable).start();
-	    }
-	
-	    public String  fechaSistema() {
-	        Calendar cal=Calendar.getInstance();
-	        java.util.Date date=cal.getTime();
-	        DateFormat dateFormatter=DateFormat.getDateInstance(DateFormat. FULL, Locale.getDefault());
-	        String fecha=dateFormatter.format(date); 
-	        return fecha;
-	    }
-	    
-	    public static void actualizarValorAleatorio() {
-			System.out.println("Actualización de comparacion");
-			int numero = calcularValor();
-			
-	    	JsonProvider provider = JsonProvider.provider();
-	        for (KPI kpi : Kpis) {
+	private int deviceId = 0;
+	private final static Set<Session> sessions = new HashSet<>();
+	private final static Set<KPI> Kpis = new HashSet<>();
+	private int TIEMPO = 5000; // en milisegundos
 
-	            JsonObject addMessage = provider.createObjectBuilder()
-		                .add("action", "actualizar")
-		                .add("id", kpi.getId())
-		                .add("comparacion", numero)
-		                .build();
-	            sendToAllConnectedSessions(addMessage);
-	        }
-	 
-	    }
-	    	    
-	    public static int calcularValor() {
-	    	int numero = (int)(Math.random()*100+1);
-	    	return numero;
-	    }
-	    
-	    public void addSession(Session session) {
-	        sessions.add(session);
-	        int valor = calcularValor();
-	        JsonProvider provider = JsonProvider.provider();
-	        for (KPI kpi : Kpis) {
-	            JsonObject addMessage = provider.createObjectBuilder()
-		                .add("action", "add")
-		                .add("id", kpi.getId())
-		                .add("name", kpi.getName())
-		                .add("type", kpi.getType())
-		                .add("name2", kpi.getName2())
-		                .add("type2", kpi.getType2())
-		                .add("comparacion", valor)
-		                .build();
-	            sendToSession(session, addMessage);
-	        }
-	    }
+	public KPISessionHandler() {
+		System.out.println("hello world!");
+		// Thread
+		main();
+	}
 
-	    public void removeSession(Session session) {
-	        sessions.remove(session);
-	    }
-	    
-	    public void addComparation(KPI kpi) {
-	    	kpi.setId(deviceId);
-	        Kpis.add(kpi);
-	        deviceId++;
-	        int valor = calcularValor();
-	        JsonProvider provider = JsonProvider.provider();
-	        JsonObject addMessage = provider.createObjectBuilder()
-	                .add("action", "add")
-	                .add("id", kpi.getId())
-	                .add("name", kpi.getName())
-	                .add("type", kpi.getType())
-	                .add("name2", kpi.getName2())
-	                .add("type2", kpi.getType2())
-	                .add("comparacion", valor)
-	                .build();
-	        sendToAllConnectedSessions(addMessage); 
-	        
-	    }
-	    
-	    public  void removeKPI(int id) {
-	    	System.out.println("KPI Eliminada");
-	    	KPI kpi = getKPIById(id);
-	        if (kpi != null) {
-	        	Kpis.remove(kpi);
-	            JsonProvider provider = JsonProvider.provider();
-	            JsonObject removeMessage = provider.createObjectBuilder()
-	                    .add("action", "remove")
-	                    .add("id", id)
-	                    .build();
-	            sendToAllConnectedSessions(removeMessage);
-	        } 
-	    }
+	public void main() {
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(TIEMPO);
+						KPISessionHandler.actualizarValorAleatorio();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
 
+		new Thread(runnable).start();
+	}
 
-	    private  KPI getKPIById(int id) {
-	        for (KPI kpi : Kpis) {
-	            if (kpi.getId() == id) {
-	                return kpi;
-	            }
-	        }
-	        return null;
-	    }
-	    
-	    public static void sendToAllConnectedSessions(JsonObject message) {  
-	        for (Session session : sessions) {
-	            sendToSession(session, message);
-	        }
-	    }
+	public static String fechaSistema() {
+		Date objDate = new Date();
+		System.out.println(objDate.toString());
+		return objDate.toString();
+	}
 
-	    public static void sendToSession(Session session, JsonObject message) {
-	        try {
-	            session.getBasicRemote().sendText(message.toString());
-	        } catch (IOException ex) {
-	            sessions.remove(session);
-	            Logger.getLogger(KPISessionHandler.class.getName()).
-	                    log(Level.SEVERE, null, ex);
-	        }
-	    }
+	public static void actualizarValorAleatorio() {
+		System.out.println("Actualización de comparacion");
+		int numero = calcularValor();
+
+		JsonProvider provider = JsonProvider.provider();
+		for (KPI kpi : Kpis) {
+			numero = calcularValor();
+			JsonObject addMessage = provider.createObjectBuilder().add("action", "actualizar").add("id", kpi.getId())
+					.add("comparacion", numero).add("modificado", fechaSistema()).build();
+			sendToAllConnectedSessions(addMessage);
+		}
+
+	}
+
+	public static int calcularValor() {
+		int numero = (int) (Math.random() * 100 + 1);
+		return numero;
+	}
+
+	public void addSession(Session session) {
+		sessions.add(session);
+		int valor = calcularValor();
+		JsonProvider provider = JsonProvider.provider();
+		for (KPI kpi : Kpis) {
+			JsonObject addMessage = provider.createObjectBuilder().add("action", "add").add("id", kpi.getId())
+					.add("name", kpi.getName()).add("type", kpi.getType()).add("name2", kpi.getName2())
+					.add("type2", kpi.getType2()).add("comparacion", valor).add("modificado", fechaSistema()).build();
+			sendToSession(session, addMessage);
+		}
+	}
+
+	public void removeSession(Session session) {
+		sessions.remove(session);
+	}
+
+	public void addComparation(KPI kpi) {
+		kpi.setId(deviceId);
+		Kpis.add(kpi);
+		deviceId++;
+		int valor = calcularValor();
+		JsonProvider provider = JsonProvider.provider();
+		JsonObject addMessage = provider.createObjectBuilder().add("action", "add").add("id", kpi.getId())
+				.add("name", kpi.getName()).add("type", kpi.getType()).add("name2", kpi.getName2())
+				.add("type2", kpi.getType2()).add("comparacion", valor).add("modificado", fechaSistema()).build();
+		sendToAllConnectedSessions(addMessage);
+
+	}
+
+	public void removeKPI(int id) {
+		System.out.println("KPI Eliminada");
+		KPI kpi = getKPIById(id);
+		if (kpi != null) {
+			Kpis.remove(kpi);
+			JsonProvider provider = JsonProvider.provider();
+			JsonObject removeMessage = provider.createObjectBuilder().add("action", "remove").add("id", id).build();
+			sendToAllConnectedSessions(removeMessage);
+		}
+	}
+
+	private KPI getKPIById(int id) {
+		for (KPI kpi : Kpis) {
+			if (kpi.getId() == id) {
+				return kpi;
+			}
+		}
+		return null;
+	}
+
+	public static void sendToAllConnectedSessions(JsonObject message) {
+		for (Session session : sessions) {
+			sendToSession(session, message);
+		}
+	}
+
+	public static void sendToSession(Session session, JsonObject message) {
+		try {
+			session.getBasicRemote().sendText(message.toString());
+		} catch (IOException ex) {
+			sessions.remove(session);
+			Logger.getLogger(KPISessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -14,10 +14,13 @@ function onMessage(event) {
         document.getElementById(kpi.id).remove();
     }
     if (kpi.action === "actualizar") {
+    	//Actualización del valor
         var node = document.getElementById(kpi.id);
         var comparationText = node.children[4];
-        comparationText.innerHTML = "<b>Comparación:</b> " + kpi.comparacion;
-       
+        comparationText.innerHTML = "<b>Comparación:</b> " + kpi.comparacion+"<br/>";
+        // Actualización de la fecha
+        var actualizaconFecha = node.children[5];
+        actualizaconFecha.innerHTML = "<b>Fecha de actualización:</b> " + kpi.modificado+"<br/>";
     }
     
 
@@ -56,32 +59,36 @@ function printKPIComparationElement(kpi){
     
     var kpiName1 = document.createElement("span");
     kpiName1.setAttribute("class", "kpiName1");
-    kpiName1.innerHTML = kpi.name;
+    kpiName1.innerHTML = kpi.name+"<br/>";
     kpiDiv.appendChild(kpiName1);
 
     var kpiType = document.createElement("span");
-    kpiType.innerHTML = "<b>Type:</b> " + kpi.type;
+    kpiType.innerHTML = "<b>Type:</b> " + kpi.type+"<br/>";
     kpiDiv.appendChild(kpiType);
     
     var kpiName2 = document.createElement("span");
     kpiName2.setAttribute("class", "kpiName2");
-    kpiName2.innerHTML = kpi.name2;
+    kpiName2.innerHTML = kpi.name2+"<br/>";
     kpiDiv.appendChild(kpiName2);
 
     var kpiType = document.createElement("span");
-    kpiType.innerHTML = "<b>Type:</b> " + kpi.type2;
+    kpiType.innerHTML = "<b>Type:</b> " + kpi.type2+"<br/>";
     kpiDiv.appendChild(kpiType);
     
     var kpiComparation = document.createElement("span");
     kpiComparation.setAttribute("id", "comp"+kpi.id);
-    kpiComparation.innerHTML = "<b>Comparación:</b> " + kpi.comparacion;
+    kpiComparation.innerHTML = "<b>Comparación:</b> " + kpi.comparacion+"<br/>";
     kpiDiv.appendChild(kpiComparation);
     
+    var fechamodificacion = document.createElement("span");
+    fechamodificacion.setAttribute("id", "act"+kpi.id);
+    fechamodificacion.innerHTML = "<b>Fecha de actualización:</b> " + kpi.modificado+"<br/>";
+    kpiDiv.appendChild(fechamodificacion);
   
     
     var removeKpi = document.createElement("span");
     removeKpi.setAttribute("class", "removeKpi");
-    removeKpi.innerHTML = "<a href=\"#\" OnClick=removeKPI(" + kpi.id + ")>Remove comparation</a>";
+    removeKpi.innerHTML = "<a href=\"#\" OnClick=removeKPI(" + kpi.id + ")>Remove comparation</a>"+"<br/>";
     kpiDiv.appendChild(removeKpi);
     
 	
@@ -89,21 +96,21 @@ function printKPIComparationElement(kpi){
 
 
 function showForm() {
-    document.getElementById("addDeviceForm").style.display = '';
+    document.getElementById("addKPIForm").style.display = '';
 }
 
 function hideForm() {
-    document.getElementById("addDeviceForm").style.display = "none";
+    document.getElementById("addKPIForm").style.display = "none";
 }
 
 function formSubmit() {
-    var form = document.getElementById("addDeviceForm");
+    var form = document.getElementById("addKPIForm");
 	var name1 = form.elements["device_name1"].value;
 	var type1 = form.elements["device_type1"].value;
     var name2 = form.elements["device_name2"].value;
     var type2 = form.elements["device_type2"].value;
     hideForm();
-    document.getElementById("addDeviceForm").reset();
+    document.getElementById("addKPIForm").reset();
     addComparation(name1,name2,type1,type2);
 }
 
